c6bdda73dd3c72b07c0d905fe737d1c1db52cca9
Luke Brown <53307399+lukelbro@users.noreply.github.com>
2025-02-26 12:45:25 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uchu',  # Package name
+    version='0.1.0',
+    description='A Python package to convert OKLCH colors to RGB and provide easy access to favorite colors.',
+    url='https://github.com/lukelbro/uchu',  # Update this
+    packages=find_packages(),
+    install_requires=[]
+)
