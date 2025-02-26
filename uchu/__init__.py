a6a6dc3e44510741de5b21a939e73e8fc906d183
Luke Brown <53307399+lukelbro@users.noreply.github.com>
2025-02-26 14:19:29 +0000

    Update __init__.py

--- uchu/__init__.py
+++ uchu/__init__.py
@@ -1,1 +1,47 @@
 from .uchu import uchu
+# Blue
+blue       = uchu.blue
+bluedark   = uchu.bluedark
+bluelight  = uchu.bluelight
+
+# General colors (non-standard names)
+yang       = uchu.yang
+yin        = uchu.generalyin
+
+# Gray
+gray       = uchu.gray
+graydark   = uchu.graydark
+graylight  = uchu.graylight
+
+# Green
+green       = uchu.green
+greendark   = uchu.greendark
+greenlight  = uchu.greenlight
+
+# Pink
+pink       = uchu.pink
+pinkdark   = uchu.pinkdark
+pinklight  = uchu.pinklight
+
+# Purple
+purple       = uchu.purple
+purpledark   = uchu.purpledark
+purplelight  = uchu.purplelight
+
+# Orange
+orange       = uchu.orange
+orangedark   = uchu.orangedark
+orangelight  = uchu.orangelight
+
+# Red
+red        = uchu.red
+reddark    = uchu.reddark
+redlight   = uchu.redlight
+
+# Yellow
+yellow       = uchu.yellow
+yellowdark   = uchu.yellowdark
+yellowlight  = uchu.yellowlight
+
+# Yin (separate from general)
+yinlight  = uchu.yin_light
